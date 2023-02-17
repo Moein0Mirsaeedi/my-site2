fd3117e8bac6269b4bd8c3b4525fd29cd72a0af5
Moein Mirsaeedi <moein.mirsaeedi@gmail.com>
2023-02-17 20:39:33 +0330

    Add change language, change color1

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+let $$ = document;
 let liElemMobileMenu = document.getElementsByClassName("nav-link");
 let navbar = document.getElementById("navbar");
 for (const key of liElemMobileMenu) {
@@ -27,3 +28,79 @@ function checkScroll() {
 }
 
 checkScroll();
+
+// change language
+
+let languages = [
+  {
+    menuItem: ["خانه", "درباره من", "خدمات", "مهارت ها", "ارتباط با من"],
+    homePage: [
+      "معین میرسعیدی",
+      `برنامه نویس ، طراح وب <br />
+                طراح گرافیک و... <br />
+                <span>تماس: 09056249543</span>`,
+    ],
+  },
+  {
+    menuItem: ["Home", "About me", "My service", "My skills", "Contact me"],
+    homePage: [
+      "Moein Mirsaeedi",
+      "Programmer , Web designer, <br>Graphic designer and...",
+      "call: 09056249543",
+    ],
+  },
+];
+
+let btnChangeLan = document.getElementsByClassName("changeLan");
+let navItem = document.getElementsByClassName("nav-link");
+let languageSite = "EN";
+
+for (const key of btnChangeLan) {
+  key.addEventListener("click", function () {
+    if (languageSite === "EN") {
+      changeToFA();
+    } else if (languageSite === "FA") {
+      changeToEN();
+    }
+  });
+}
+
+// Change language to FA
+function changeToFA() {
+  languageSite = "FA";
+  $$.querySelector("#changeLan").innerHTML = "EN";
+
+  // menu change
+  document.body.style.direction = "rtl";
+  for (const key in navItem) {
+    navItem[key].innerHTML = languages[0].menuItem[key];
+  }
+  $$.querySelector(".navbar-brand").classList.add("ms-4");
+  $$.querySelector("#changeLan").classList.remove("ms-auto");
+  $$.querySelector("#changeLan").classList.add("me-auto");
+  $$.querySelector("#changeLan").classList.add("ms-2");
+
+  // home page change
+  $$.querySelector(".home-name").innerHTML = languages[0].homePage[0];
+  $$.querySelector(".home-about-me").innerHTML = languages[0].homePage[1];
+}
+
+// Change language to EN
+function changeToEN() {
+  languageSite = "EN";
+  $$.querySelector("#changeLan").innerHTML = "FA";
+
+  // menu change
+  document.body.style.direction = "ltr";
+  for (const key in navItem) {
+    navItem[key].innerHTML = languages[1].menuItem[key];
+  }
+  $$.querySelector(".navbar-brand").classList.add("ms-4");
+  $$.querySelector("#changeLan").classList.add("ms-auto");
+  $$.querySelector("#changeLan").classList.remove("me-auto");
+  $$.querySelector("#changeLan").classList.remove("ms-2");
+
+  // home page change
+  $$.querySelector(".home-name").innerHTML = languages[1].homePage[0];
+  $$.querySelector(".home-about-me").innerHTML = languages[1].homePage[1];
+}
